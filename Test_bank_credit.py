6d710f9e3dcb74fa6cc1413142fe5e05ee04a466
admin <admin@localhost>
2018-11-27 17:23:31 +0300

    Test_bank_credit added

---
+++ Test_bank_credit.py
@@ -0,0 +1,16 @@
+import pytest
+
+from Bank_credit import calculation_of_monthly_payment_in_rub
+
+
+@pytest.mark.parametrize('sum_of_credit_in_rub, rate_of_credit, time_of_credit_in_month, expected', [
+    (300000, 0.2, 36, 11149),
+    (450000, 0.15, 18, 28073),
+    (75000, 0.18, 12, 6876)
+
+])
+def test_calculation_of_monthly_payment_in_rub(sum_of_credit_in_rub, rate_of_credit, time_of_credit_in_month, expected):
+    actual = calculation_of_monthly_payment_in_rub(sum_of_credit_in_rub, rate_of_credit, time_of_credit_in_month)
+    assert expected == actual
+
+
